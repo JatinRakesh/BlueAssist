6ac49aea251fc4be5cdc8cd5aa0c6dcf2e74d249
JatinRakesh <jatinrakesh15@gmail.com>
2026-05-07 22:22:03 +0800

    Initial Commit

---
+++ BlueAssistMacTests/BlueAssistMacTests.swift
@@ -0,0 +1,19 @@
+//
+//  BlueAssistMacTests.swift
+//  BlueAssistMacTests
+//
+//  Created by Jatin Rakesh on 7/5/26.
+//
+
+import Testing
+@testable import BlueAssistMac
+
+struct BlueAssistMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
